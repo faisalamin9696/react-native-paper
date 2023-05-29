7c20c5332fc34f2012d739c4a32aaee304b12fe2
faisalamin9696 <faisalamin9696@gmail.com>
2023-05-29 13:53:36 +0500

    spacing and round segment

--- src/components/SegmentedButtons/SegmentedButtons.tsx
+++ src/components/SegmentedButtons/SegmentedButtons.tsx
@@ -1,198 +1,251 @@
 import * as React from 'react';
 import {
+  Animated,
   GestureResponderEvent,
   StyleProp,
   StyleSheet,
+  TextStyle,
   View,
   ViewStyle,
 } from 'react-native';
 
+import color from 'color';
 import type { ThemeProp } from 'src/types';
 
 import { useInternalTheme } from '../../core/theming';
 import type { IconSource } from '../Icon';
-import SegmentedButtonItem from './SegmentedButtonItem';
-import { getDisabledSegmentedButtonStyle } from './utils';
-
-type ConditionalValue =
-  | {
-      /**
-       * Array of the currently selected segmented button values.
-       */
-      value: string[];
-      /**
-       * Support multiple selected options.
-       */
-      multiSelect: true;
-      /**
-       * Function to execute on selection change
-       */
-      onValueChange: (value: string[]) => void;
-    }
-  | {
-      /**
-       * Value of the currently selected segmented button.
-       */
-      value: string;
-      /**
-       * Support multiple selected options.
-       */
-      multiSelect?: false;
-      /**
-       * Function to execute on selection change
-       */
-      onValueChange: (value: string) => void;
-    };
+import Icon from '../Icon';
+import TouchableRipple from '../TouchableRipple/TouchableRipple';
+import Text from '../Typography/Text';
+import {
+  getSegmentedButtonBorderRadius,
+  getSegmentedButtonColors,
+  getSegmentedButtonDensityPadding,
+} from './utils';
 
 export type Props = {
   /**
-   * Buttons to display as options in toggle button.
-   * Button should contain the following properties:
-   * - `value`: value of button (required)
-   * - `icon`: icon to display for the item
-   * - `disabled`: whether the button is disabled
-   * - `accessibilityLabel`: acccessibility label for the button. This is read by the screen reader when the user taps the button.
-   * - `checkedColor`: custom color for checked Text and Icon
-   * - `uncheckedColor`: custom color for unchecked Text and Icon
-   * - `onPress`: callback that is called when button is pressed
-   * - `label`: label text of the button
-   * - `showSelectedCheck`: show optional check icon to indicate selected state
-   * - `style`: pass additional styles for the button
-   * - `testID`: testID to be used on tests
+   * Whether the segmented button is checked
    */
-  buttons: {
-    value: string;
-    icon?: IconSource;
-    disabled?: boolean;
-    accessibilityLabel?: string;
-    checkedColor?: string;
-    uncheckedColor?: string;
-    onPress?: (event: GestureResponderEvent) => void;
-    label?: string;
-    showSelectedCheck?: boolean;
-    style?: StyleProp<ViewStyle>;
-    testID?: string;
-  }[];
+  checked: boolean;
   /**
-   * Density is applied to the height, to allow usage in denser UIs
+   * Icon to display for the `SegmentedButtonItem`.
+   */
+  icon?: IconSource;
+
+  /**
+   * @supported Available in v5.x with theme version 3
+   * Custom color for unchecked Text and Icon.
+   */
+  uncheckedColor?: string;
+
+  /**
+   * @supported Available in v5.x with theme version 3
+   * Custom color for checked Text and Icon.
+   */
+  checkedColor?: string;
+  /**
+   * Whether the button is disabled.
+   */
+  disabled?: boolean;
+  /**
+   * Accessibility label for the `SegmentedButtonItem`. This is read by the screen reader when the user taps the button.
+   */
+  accessibilityLabel?: string;
+  /**
+   * Function to execute on press.
+   */
+  onPress?: (event: GestureResponderEvent) => void;
+  /**
+   * Value of button.
+   */
+  value: string;
+  /**
+   * Label text of the button.
+   */
+  label?: string;
+  /**
+   * Button segment.
+   */
+  segment?: 'first' | 'last';
+  /**
+   * Show optional check icon to indicate selected state
+   */
+  showSelectedCheck?: boolean;
+  /**
+   * Density is applied to the height, to allow usage in denser UIs.
    */
   density?: 'regular' | 'small' | 'medium' | 'high';
   style?: StyleProp<ViewStyle>;
+  /**
+   * testID to be used on tests.
+   */
+  testID?: string;
+  /**
+   * @optional
+   */
   theme?: ThemeProp;
-} & ConditionalValue;
+};
 
-/**
- * Segmented buttons can be used to select options, switch views or sort elements.</br>
- *
- * <div class="screenshots">
- *   <img class="medium" src="screenshots/segmented-button.png" />
- * </div>
- *
- * ## Usage
- * ```js
- * import * as React from 'react';
- * import { SafeAreaView, StyleSheet } from 'react-native';
- * import { SegmentedButtons } from 'react-native-paper';
- *
- * const MyComponent = () => {
- *   const [value, setValue] = React.useState('');
- *
- *   return (
- *     <SafeAreaView style={styles.container}>
- *       <SegmentedButtons
- *         value={value}
- *         onValueChange={setValue}
- *         buttons={[
- *           {
- *             value: 'walk',
- *             label: 'Walking',
- *           },
- *           {
- *             value: 'train',
- *             label: 'Transit',
- *           },
- *           { value: 'drive', label: 'Driving' },
- *         ]}
- *       />
- *     </SafeAreaView>
- *   );
- * };
- *
- * const styles = StyleSheet.create({
- *   container: {
- *     flex: 1,
- *     alignItems: 'center',
- *   },
- * });
- *
- * export default MyComponent;
- *```
- */
-const SegmentedButtons = ({
-  value,
-  onValueChange,
-  buttons,
-  multiSelect,
-  density,
+const SegmentedButtonItem = ({
+  checked,
+  accessibilityLabel,
+  disabled,
   style,
+  showSelectedCheck,
+  checkedColor,
+  uncheckedColor,
+  icon,
+  testID,
+  label,
+  onPress,
+  segment,
+  density = 'regular',
   theme: themeOverrides,
 }: Props) => {
   const theme = useInternalTheme(themeOverrides);
 
+  const checkScale = React.useRef(new Animated.Value(0)).current;
+
+  React.useEffect(() => {
+    if (!showSelectedCheck) {
+      return;
+    }
+    if (checked) {
+      Animated.spring(checkScale, {
+        toValue: 1,
+        useNativeDriver: true,
+      }).start();
+    } else {
+      Animated.spring(checkScale, {
+        toValue: 0,
+        useNativeDriver: true,
+      }).start();
+    }
+  }, [checked, checkScale, showSelectedCheck]);
+
+  const { roundness, isV3 } = theme;
+  const { borderColor, textColor, borderWidth, backgroundColor } =
+    getSegmentedButtonColors({
+      checked,
+      theme,
+      disabled,
+      checkedColor,
+      uncheckedColor,
+    });
+
+  const borderRadius = (isV3 ? 5 : 1) * roundness;
+  const segmentBorderRadius = getSegmentedButtonBorderRadius({
+    theme,
+    segment,
+  });
+  const rippleColor = color(textColor).alpha(0.12).rgb().string();
+
+  const showIcon = !icon ? false : label && checked ? !showSelectedCheck : true;
+  const showCheckedIcon = checked && showSelectedCheck;
+
+  const iconSize = isV3 ? 18 : 16;
+  const iconStyle = {
+    marginRight: label ? 5 : showCheckedIcon ? 3 : 0,
+    ...(label && {
+      transform: [
+        {
+          scale: checkScale.interpolate({
+            inputRange: [0, 1],
+            outputRange: [1, 0],
+          }),
+        },
+      ],
+    }),
+  };
+
+  const buttonStyle: ViewStyle = {
+    backgroundColor,
+    borderColor,
+    borderWidth,
+    borderRadius,
+
+  };
+  const paddingVertical = getSegmentedButtonDensityPadding({ density });
+  const rippleStyle: ViewStyle = {
+    borderRadius,
+    ...segmentBorderRadius,
+  };
+  const labelTextStyle: TextStyle = {
+    ...(!isV3
+      ? {
+        textTransform: 'uppercase',
+        fontWeight: '500',
+
+      }
+      : theme.fonts.labelLarge),
+    color: textColor,
+    fontSize: 10
+
+  };
+
   return (
-    <View style={[styles.row, style]}>
-      {buttons.map((item, i) => {
-        const disabledChildStyle = getDisabledSegmentedButtonStyle({
-          theme,
-          buttons,
-          index: i,
-        });
-        const segment =
-          i === 0 ? 'first' : i === buttons.length - 1 ? 'last' : undefined;
+    <View style={[buttonStyle, styles.button, style]}>
 
-        const checked =
-          multiSelect && Array.isArray(value)
-            ? value.includes(item.value)
-            : value === item.value;
 
-        const onPress = (e: GestureResponderEvent) => {
-          item.onPress?.(e);
-
-          const nextValue =
-            multiSelect && Array.isArray(value)
-              ? checked
-                ? value.filter((val) => item.value !== val)
-                : [...value, item.value]
-              : item.value;
-
-          // @ts-expect-error: TS doesn't preserve types after destructuring, so the type isn't inferred correctly
-          onValueChange(nextValue);
-        };
-
-        return (
-          <SegmentedButtonItem
-            {...item}
-            key={i}
-            checked={checked}
-            segment={segment}
-            density={density}
-            onPress={onPress}
-            style={[item.style, disabledChildStyle]}
-            theme={theme}
-          />
-        );
-      })}
+      <TouchableRipple
+        borderless
+        onPress={onPress}
+        accessibilityLabel={accessibilityLabel}
+        accessibilityState={{ disabled, checked }}
+        accessibilityRole="button"
+        disabled={disabled}
+        rippleColor={rippleColor}
+        testID={testID}
+        style={rippleStyle}
+        theme={theme}
+      >
+        <View style={[styles.content, { paddingVertical }]}>
+          {showCheckedIcon ? (
+            <Animated.View
+              testID={`${testID}-check-icon`}
+              style={[iconStyle, { transform: [{ scale: checkScale }] }]}
+            >
+              <Icon source={'check'} size={iconSize} color={textColor} />
+            </Animated.View>
+          ) : null}
+          {showIcon ? (
+            <Animated.View testID={`${testID}-icon`} style={iconStyle}>
+              <Icon source={icon} size={iconSize} color={textColor} />
+            </Animated.View>
+          ) : null}
+          <Text
+            variant="labelLarge"
+            style={[styles.label, labelTextStyle]}
+            selectable={false}
+            numberOfLines={1}
+          >
+            {label}
+          </Text>
+        </View>
+      </TouchableRipple>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  row: {
+  button: {
+    flex: 1,
+    minWidth: 76,
+    borderStyle: 'solid',
+  },
+  label: {
+    textAlign: 'center',
+  },
+  content: {
     flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 9,
+    paddingHorizontal: 16,
   },
 });
 
-export default SegmentedButtons;
+export default SegmentedButtonItem;
 
-// @component-docs ignore-next-line
-export { SegmentedButtons as SegmentedButtons };
+export { SegmentedButtonItem as SegmentedButton };
